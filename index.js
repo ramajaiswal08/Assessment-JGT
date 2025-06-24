6ae31fd2850e6c1617ca6a56bc740d15aa671e08
ramajaiswal08 <ramajaiswal829111@gmail.com>
2025-06-24 14:49:11 +0530

    fixed

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function loadRecommendationCards() {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 5000000 ,
         dots: true,
         arrows: true,
         responsive: [
